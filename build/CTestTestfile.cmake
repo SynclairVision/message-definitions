22332abb2d2223b67e71a84f0a6bbc4f35fc9fcc
fredriktegnell <fredrik.tegnell@hotmail.com>
2025-03-12 09:31:22 +0000

    Your commit message

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/synclairuser/message-definitions
+# Build directory: /home/synclairuser/message-definitions/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MsgDefsTest "/home/synclairuser/message-definitions/build/msg_defs_test_program")
+set_tests_properties(MsgDefsTest PROPERTIES  _BACKTRACE_TRIPLES "/home/synclairuser/message-definitions/CMakeLists.txt;14;add_test;/home/synclairuser/message-definitions/CMakeLists.txt;0;")
