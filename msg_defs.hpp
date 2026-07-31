b4c3974b9574cdfa4afd73fa91e02c48ef79a80e
tellurain <wbblbbdbdb@outlook.com>
2026-07-31 11:51:12 +0200

    value type fixes

--- msg_defs.hpp
+++ msg_defs.hpp
@@ -731,7 +731,7 @@ inline void pack_navigation_parameters(
     message &msg, float altitude, float visual_lat = 0.0f, float visual_lon = 0.0f,
     float next_waypoint_target_yaw = 0.0f, float next_waypoint_target_pitch = 0.0f,
     float next_waypoint_target_roll = 0.0f, float visual_vel_x = 0.0f,
-    float visual_vel_y = 0.0f, float visual_vel_z = 0.0f) {
+    float visual_vel_y = 0.0f, float visual_vel_z = 0.0f, float desired_thrust = 0.0f, uint8_t position_quality = 0U) {
     msg.param_type = NAVIGATION;
     uint16_t offset = 0;
     int32_t mm;
@@ -770,6 +770,13 @@ inline void pack_navigation_parameters(
 
     mm = static_cast<int32_t>(visual_vel_z * 1000.0f);
     memcpy((void *)&msg.data[offset], &mm, sizeof(int32_t));
+    offset += sizeof(int32_t);
+
+    mm = static_cast<int32_t>(desired_thrust * 1000.0f);
+    memcpy((void *)&msg.data[offset], &mm, sizeof(int32_t));
+    offset += sizeof(int32_t);
+
+    memcpy(&msg.data[offset], &position_quality, sizeof(position_quality));
 }
 
 inline void pack_debug_parameters(
